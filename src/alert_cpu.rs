8cd17ff774cffeebd38fd8ac78dd4c976f5806d9
Doug Ilijev <doug.ilijev@gmail.com>
2025-01-28 20:13:49 -0800

    [style] Fix indentation

--- src/alert_cpu.rs
+++ src/alert_cpu.rs
@@ -21,65 +21,65 @@ impl CpuMonitor for System {
 }
 
 pub fn evolve_cpu_state<T: CpuMonitor>(
-  sys: &mut T,
-  current_state: CpuState,
-  threshold: f32,
-  above_threshold_count: &mut i32,
-  below_threshold_count: &mut i32,
+    sys: &mut T,
+    current_state: CpuState,
+    threshold: f32,
+    above_threshold_count: &mut i32,
+    below_threshold_count: &mut i32,
 ) -> (CpuState, bool, f32, bool) {
-  let cpu_usage = sys.get_cpu_usage();
-  let mut next_state = current_state;
-  let mut play_alert = false;
-  let mut display_log = false;
+    let cpu_usage = sys.get_cpu_usage();
+    let mut next_state = current_state;
+    let mut play_alert = false;
+    let mut display_log = false;
 
-  match current_state {
-      CpuState::Initial => {
-          if cpu_usage > threshold {
-              next_state = CpuState::RisingEdge;
-          } else {
-              next_state = CpuState::BelowThreshold;
-          }
-          display_log = true;
-      }
-      CpuState::RisingEdge => {
-          if cpu_usage > threshold {
-              *above_threshold_count += 1;
-              if *above_threshold_count >= 1 {
-                  next_state = CpuState::OverThreshold;
-              }
-          } else {
-              *above_threshold_count = 0;
-              next_state = CpuState::BelowThreshold;
-          }
-          display_log = true;
-      }
-      CpuState::OverThreshold => {
-          if cpu_usage <= threshold {
-              next_state = CpuState::FallingEdge;
-              display_log = true;
-          }
-      }
-      CpuState::FallingEdge => {
-          if cpu_usage <= threshold {
-              *below_threshold_count += 1;
-              if *below_threshold_count >= 1 {
-                  next_state = CpuState::BelowThreshold;
-                  play_alert = true;
-              }
-          } else {
-              *below_threshold_count = 0;
-              next_state = CpuState::OverThreshold;
-          }
-          display_log = true;
-      }
-      CpuState::BelowThreshold => {
-          if cpu_usage > threshold {
-              *above_threshold_count += 1;
-              next_state = CpuState::RisingEdge;
-              display_log = true;
-          }
-      }
-  }
+    match current_state {
+        CpuState::Initial => {
+            if cpu_usage > threshold {
+                next_state = CpuState::RisingEdge;
+            } else {
+                next_state = CpuState::BelowThreshold;
+            }
+            display_log = true;
+        }
+        CpuState::RisingEdge => {
+            if cpu_usage > threshold {
+                *above_threshold_count += 1;
+                if *above_threshold_count >= 1 {
+                    next_state = CpuState::OverThreshold;
+                }
+            } else {
+                *above_threshold_count = 0;
+                next_state = CpuState::BelowThreshold;
+            }
+            display_log = true;
+        }
+        CpuState::OverThreshold => {
+            if cpu_usage <= threshold {
+                next_state = CpuState::FallingEdge;
+                display_log = true;
+            }
+        }
+        CpuState::FallingEdge => {
+            if cpu_usage <= threshold {
+                *below_threshold_count += 1;
+                if *below_threshold_count >= 1 {
+                    next_state = CpuState::BelowThreshold;
+                    play_alert = true;
+                }
+            } else {
+                *below_threshold_count = 0;
+                next_state = CpuState::OverThreshold;
+            }
+            display_log = true;
+        }
+        CpuState::BelowThreshold => {
+            if cpu_usage > threshold {
+                *above_threshold_count += 1;
+                next_state = CpuState::RisingEdge;
+                display_log = true;
+            }
+        }
+    }
 
-  (next_state, play_alert, cpu_usage, display_log)
+    (next_state, play_alert, cpu_usage, display_log)
 }
