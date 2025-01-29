799668ef0b75d18b788a27dfc29a0907d989451e
Doug Ilijev <doug.ilijev@gmail.com>
2025-01-28 20:11:18 -0800

    Add another test to exercise staying longer in the high and low states

--- tests/cpu_monitor_tests.rs
+++ tests/cpu_monitor_tests.rs
@@ -24,8 +24,9 @@ impl CpuMonitor for MockCpuMonitor {
     }
 }
 
+/// Basic test of state transitions.
 #[test]
-fn test_evolve_cpu_state() {
+fn test_evolve_cpu_state_basics() {
     let usage_pattern = vec![5.0, 25.0, 30.0, 15.0, 10.0, 5.0, 25.0, 30.0];
     let mut mock_monitor = MockCpuMonitor::new(usage_pattern);
 
@@ -34,6 +35,51 @@ fn test_evolve_cpu_state() {
         CpuState::RisingEdge,
         CpuState::OverThreshold,
         CpuState::FallingEdge,
+        CpuState::BelowThreshold,
+        CpuState::BelowThreshold,
+        CpuState::RisingEdge,
+        CpuState::OverThreshold,
+    ];
+
+    let threshold = 20.0;
+    let mut above_threshold_count = 0;
+    let mut below_threshold_count = 0;
+
+    let mut state = CpuState::Initial;
+    println!("State: {:?}", state);
+
+    for i in 0..mock_monitor.usage_pattern.len() {
+        let (next_state, _play_alert, cpu_usage, _display_log) =
+            evolve_cpu_state(
+                &mut mock_monitor,
+                state,
+                threshold,
+                &mut above_threshold_count,
+                &mut below_threshold_count);
+
+        println!("{:?} \t -> {:2} -> \t {:?}", state, cpu_usage, next_state);
+
+        state = next_state;
+        assert_eq!(next_state, state_pattern[i]);
+    }
+
+    // Add assertions as needed to verify the state transitions and behavior
+    assert_eq!(state, CpuState::RisingEdge);
+}
+
+/// Stay longer in the OverThreshold and BelowThreshold states.
+#[test]
+fn test_evolve_cpu_state_longer_stay() {
+    let usage_pattern = vec![5.0, 25.0, 30.0, 35.0, 15.0, 10.0, 5.0, 8.0, 25.0, 30.0];
+    let mut mock_monitor = MockCpuMonitor::new(usage_pattern);
+
+    let state_pattern = vec![
+        CpuState::BelowThreshold,
+        CpuState::RisingEdge,
+        CpuState::OverThreshold,
+        CpuState::OverThreshold,
+        CpuState::FallingEdge,
+        CpuState::BelowThreshold,
         CpuState::BelowThreshold,
         CpuState::BelowThreshold,
         CpuState::RisingEdge,
